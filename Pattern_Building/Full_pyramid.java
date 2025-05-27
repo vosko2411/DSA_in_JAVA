f361c3c136f3a51b0619e077aae393c4b7468de8
Prajjwal Dwivedi <prajjwald59@gmail.com>
2025-05-27 19:42:34 +0530

    pattern

--- Pattern_Building/Full_pyramid.java
+++ Pattern_Building/Full_pyramid.java
@@ -20,17 +20,6 @@ public class Full_pyramid {
             System.out.println();
         }
 
-        for(int i=1;i<=n;i++)
-        {
-            
-            for(int j=1;j<=i;j++){
-                System.out.print(" ");
-            }
-            for(int k=i+1;k<2*n-i;k++)
-            {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        
     }
 }
